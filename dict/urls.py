2cfc4be50a3db8b8a55e86c42f3401069f226544
ASHISH <ashishaxle207@gmail.com>
2023-02-20 10:26:19 +0530

    Commit

--- dict/urls.py
+++ dict/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from dict import views
 
 urlpatterns = [
-    path('',views.home,name = 'home'),
+    path('',views.login_page,name = 'login'),
     path('register',views.register,name = 'register'),
     path('mainpage',views.mainpage,name = 'mainpage'),
+    path('logout',views.logout,name = 'logout'),
 ]
--- dict/views.py
+++ dict/views.py
@@ -1,13 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
 
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
-def home(request):
-    return render(request,'Dict/home.html')
-
 def register(request):
-    return render(request, 'Dict/registration.html')
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for' + user)
+
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'Dict/register.html', context)
+
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('mainpage')
+        else:
+            messages.info(request, 'Username or Password  is Incorrect')
+
+    return render(request, 'Dict/login.html')
+
 
 def mainpage(request):
     return render(request, 'Dict/mainpage.html')
+
+def logout(request):
+    return redirect('/')
